de90810af73d4d9c203c842aba5154d10bce1ee0
DarkLightWindraK <DarkLightWindraK@yandex.ru>
2023-03-10 22:57:40 +0300

    Make the adapter pattern

--- Labwork1OOD/main.swift
+++ Labwork1OOD/main.swift
@@ -1,11 +1,53 @@
-//
-//  main.swift
-//  Labwork1OOD
-//
-//  Created by Антон Смирнов on 10.03.2023.
-//
-
 import Foundation
 
-print("Hello, World!")
+protocol PrintableAllInfo {
+    func getAllInfo() -> String
+}
+
+protocol PrintableDifferentInfo {
+    func getFIO() -> String
+    func getBirthday() -> String
+}
+
+class PrinterAllInfo: PrintableAllInfo {
+    func getAllInfo() -> String {
+        "Smirnov A.A. 24.04.2002"
+    }
+}
+
+class PrinterDifferentInfo: PrintableDifferentInfo {
+    func getFIO() -> String {
+        "Smirnov Anton Andreevich"
+    }
+    func getBirthday() -> String {
+        "24.04.2002"
+    }
+}
+
+class PrinterService {
+    func printInfo(printer: PrintableAllInfo) {
+        print(printer.getAllInfo())
+    }
+}
+
+class Adapter : PrintableAllInfo {
+    private let adaptee: PrintableDifferentInfo
+    
+    init(printer: PrintableDifferentInfo) {
+        adaptee = printer
+    }
+    
+    func getAllInfo() -> String {
+        "\(adaptee.getFIO()) \(adaptee.getBirthday())"
+    }
+}
+
+let service = PrinterService()
+let printerAll = PrinterAllInfo()
+let printerDifferent = PrinterDifferentInfo()
+
+service.printInfo(printer: printerAll)
+service.printInfo(printer: Adapter(printer: printerDifferent))
+
+
 
